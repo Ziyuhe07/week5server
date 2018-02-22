1c2366116634f1888f925bb30fa014d5d40d8b5c
Ziyuhe07 <ucfazhe@ucl.ac.uk>
2018-02-22 10:40:35 -0500

    Test AJAX

--- testAJAX.js
+++ testAJAX.js
@@ -30,9 +30,9 @@ function processDivChange(){
 	if (xhr.readyState <4)   //while waiting response from server
 	document.getElementById('div1').innerHTML = "loading...";
 	
-	else if (xhr.readyState <4){    //4 = Response from server has been completely loaded.
+	else if (xhr.readyState == 4){    //4 = Response from server has been completely loaded.
 		if (xhr.status == 200 && xhr.status < 300)
 			//http status between 200 to 299 are all successful
-		document.getElementById('div1'.innerHTML = xhr.responseText;
+		document.getElementById('div1').innerHTML = xhr.responseText;
 	}
 }
