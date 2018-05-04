bce7e0c6a6a004ed4af74002b96e6ed27470991d
Ziyehe <ziyu.he99@gmail.com>
2018-05-04 17:47:33 -0400

    http server

--- ucfazhe/platforms/browser/www/js/appActivity.js
+++ ucfazhe/platforms/browser/www/js/appActivity.js
@@ -15,21 +15,21 @@ function replaceGraphs() {
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		id: 'mapbox.streets'
 	}).addTo(mymap);
-	var client;
+	var clientA;
 	// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
 	var formDatalayer;
 //getPOI -view data as GeoJSON
 	function showFormData() {
-		client.open('GET','http://developer.cege.ucl.ac.uk:30271/getPOI');
-		client.onreadystatechange = showFormDataResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
-		client.send();
+		clientA.open('GET','http://developer.cege.ucl.ac.uk:30271/getPOI');
+		clientA.onreadystatechange = showFormDataResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
+		clientA.send();
 	}
 	// create the code to wait for the response from the data server, and process the response once it is received
 	function showFormDataResponse() {
 		// this function listens out for the server to say that the data is ready - i.e. has state 4
-	if (client.readyState == 4) {
+	if (clientA.readyState == 4) {
 	// once the data is ready, process the data
-	var formData = client.responseText;
+	var formData = clientA.responseText;
 	loadFormDatalayer(formData);
 	}
 	}
@@ -42,22 +42,22 @@ function replaceGraphs() {
 	// change the map zoom so that all the data is shown
 	mymap.fitBounds(formDatalayer.getBounds());
 	}
-	
+	var clientB;
 	// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
 	var earthquakelayer;
 	// create the code to get the Earthquakes data using an XMLHttpRequest
 	function getEarthquakes() {
-		client = new XMLHttpRequest();
-		client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
-		client.onreadystatechange = earthquakeResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
-		client.send();
+		clientB = new XMLHttpRequest();
+		clientB.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
+		clientB.onreadystatechange = earthquakeResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
+		clientB.send();
 	}
 	// create the code to wait for the response from the data server, and process the response once it is received
 	function earthquakeResponse() {
 	// this function listens out for the server to say that the data is ready - i.e. has state 4
-	if (client.readyState == 4) {
+	if (clientB.readyState == 4) {
 	// once the data is ready, process the data
-	var earthquakedata = client.responseText;
+	var earthquakedata = clientB.responseText;
 	loadEarthquakelayer(earthquakedata);
 	}
 	}
