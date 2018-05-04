89fb6365768a8ea0e1195a12ea1777e0c6c51461
Ziyehe <ziyu.he99@gmail.com>
2018-05-04 14:01:42 -0400

    View data as GeoJSON

--- httpServer.js
+++ httpServer.js
@@ -33,7 +33,33 @@ var fs = require('fs');
 	app.get('/',function (req,res) {
 		res.send("hello world from the HTTP server");
 	});
-
+	
+	app.get('/getPOI', function (req,res) {
+		pool.connect(function(err,client,done) {
+			if(err){
+				console.log("not able to get connection "+ err);
+				res.status(400).send(err);
+				}
+		// use the inbuilt geoJSON functionality
+		// and create the required geoJSON format using a query adapted from here: http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+		// note that query needs to be a single string with no line breaks so built it up bit by bit
+	var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
+	querystring = querystring + "(SELECT 'Feature' As type , ST_AsGeoJSON(lg.geom)::json As geometry, ";
+	querystring = querystring + "row_to_json((SELECT l FROM (SELECT id, name, category) As l )) As properties";
+	querystring = querystring + " FROM united_kingdom_poi As lg limit 100 ) As f ";
+	console.log(querystring);
+	client.query(querystring,function(err,result){
+	//call `done()` to release the client back to the pool
+	done();
+	if(err){
+		console.log(err);
+		res.status(400).send(err);
+		}
+		res.status(200).send(result.rows);
+		});
+	  });
+	});
+	
 	// read in the file and force it to be a string by adding “” at the beginning
 	var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
 	//now cononvert the configruation file into the correct format -i.e. a name/value pair array
